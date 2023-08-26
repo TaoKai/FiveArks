3f8b53a2e1938bdb1b1fa06ca81344927d670ea9
root <root@instance-2.c.charged-library-396416.internal>
2023-08-26 01:38:54 +0000

    add pages

--- server.py
+++ server.py
@@ -5,8 +5,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import threading, queue, uvicorn, json, time
 import os, sys
-import pymysql
-#scp ChenLi.tar root@34.95.204.226:/root/workspace 35.202.98.116
+#scp ChenLi.tar root@34.95.204.226:/root/workspace
 base_path = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -29,7 +28,39 @@ async def sign_up(request: Request, first_name: Annotated[str, Form()], last_nam
 
 @app.post("/answer_q1")
 async def answer_q1(request: Request, informations: Annotated[str, Form()]):
-    return templates.TemplateResponse("temp.html", {"request": request, "informations_q1": informations})
+    information_list = informations.split('|')
+    recruit1 = information_list[4] if len(information_list) > 4 else ""
+    recruit2 = information_list[5] if len(information_list) > 5 else ""
+    recruit3 = information_list[6] if len(information_list) > 6 else ""
+    
+    dic = {
+        "request": request,
+        "recruit1": recruit1,
+        "recruit2": recruit2,
+        "recruit3": recruit3,
+    }
+    return templates.TemplateResponse("question2345.html", dic)
+
+@app.post("/answer_q2345")
+async def answer_q2345(request: Request, informations: Annotated[str, Form()]):
+    information_list = informations.split('|')
+    college1 = information_list[7] if len(information_list) > 7 else ""
+    college2 = information_list[8] if len(information_list) > 8 else ""
+    college3 = information_list[9] if len(information_list) > 9 else ""
+    industry1 = information_list[10] if len(information_list) > 10 else ""
+    industry2 = information_list[11] if len(information_list) > 11 else ""
+    industry3 = information_list[12] if len(information_list) > 12 else ""
+
+    dic = {
+        "request": request,
+        "college1": college1,
+        "college2": college2,
+        "college3": college3,
+        "industry1": industry1,
+        "industry2": industry2,
+        "industry3": industry3,
+    }
+    return templates.TemplateResponse("temp.html", dic)
 
 if __name__=="__main__":
     uvicorn.run(app, host='0.0.0.0', port=80, workers=1)
